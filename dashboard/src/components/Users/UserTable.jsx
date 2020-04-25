4e41cc77a1d6e61afaabee67ef237877ab7c56ba
ANTOSzbk <antoos123@gmail.com>
2020-04-25 10:55:48 +0200

    Add backwards sort by username [Z-A]

--- dashboard/src/components/Users/UserTable.jsx
+++ dashboard/src/components/Users/UserTable.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import Users from './Users';
 import { Table } from 'react-bootstrap';
+import { connect } from 'react-redux';
 
-export default function UserTable(props) {
+function UserTable(props) {
   return (
     <Table striped bordered hover>
       <thead>
         <tr>
           <th>ID</th>
           <th>Name</th>
-          <th>Username</th>
+          <th>
+            Username{' '}
+            {props.sortedByUsername !== undefined
+              ? props.sortedByUsername
+                ? '⬆️'
+                : '⬇️'
+              : null}
+          </th>
           <th>City</th>
           <th>Email</th>
           <th>Edit</th>
@@ -22,3 +30,11 @@ export default function UserTable(props) {
     </Table>
   );
 }
+
+const mapStateToProps = (state) => {
+  return {
+    sortedByUsername: state.getUsers.sortedByUsername,
+  };
+};
+
+export default connect(mapStateToProps, null)(UserTable);
--- dashboard/src/redux/reducers/UserList.js
+++ dashboard/src/redux/reducers/UserList.js
@@ -2,7 +2,8 @@ import { GET_USERS, SET_USERS, PUSH_USER, IS_EMPTY, SORT_BY_USERNAME } from "../
 
 const initialState = {
     items: [],
-    success: false
+    success: false,
+    sortedByUsername: undefined
 }
 
 const userListReducer = (state = initialState, action) => {
@@ -27,8 +28,8 @@ const userListReducer = (state = initialState, action) => {
         case SORT_BY_USERNAME:
             return {
                 ...state,
-                items: state.items.sort((a, b) => (a.username > b.username) ? 1 : -1),
-                sortedByUsername: true
+                items: state.sortedByUsername ? state.items.sort((a, b) => (a.username > b.username) ? 1 : -1) : state.items.sort((b, a) => (a.username > b.username) ? 1 : -1),
+                sortedByUsername: !state.sortedByUsername
             }
         case IS_EMPTY:
             return {
